12d90294abec0fe87b7559888310938c5f5301c4
Genysys <samuel.b.dare@gmail.com>
2022-06-20 18:55:16 +0400

    chore: landbank test boiler plate

---
+++ contracts/src/test/LandBank.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.10;
+
+import "ds-test/test.sol";
+import "forge-std/Test.sol";
+import "forge-std/Vm.sol";
+import "../LandBank.sol";
+
+
+contract LandBankTest is Test {
+    
+}
