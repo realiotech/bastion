79aa4d3e45fc3a118919e92242c38bc89738b95d
Genysys <samuel.b.dare@gmail.com>
2022-08-20 21:28:13 +0400

    refactor: split provider out

--- app/src/lib.rs
+++ app/src/lib.rs
@@ -28,18 +28,16 @@ pub struct User {
     pub address: Address,
 }
 
-#[derive(Deserialize, Serialize)]
+// #[derive(Deserialize, Serialize)]
 pub struct DataState {
-    pub token: String,
-    pub land_contract: String,
-    pub rpc_url: String,
-    pub key: String,
+    pub token:  ERC20<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>,
+    pub land_contract:  LandNFT<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>,
+    // pub rpc_url: String,
+    // pub key: String,
+    pub provider: Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>,
 }
 
-#[derive(Deserialize, Serialize)]
-struct AppState {
-    datas: Mutex<DataState>,
-}
+
 
 #[derive(Deserialize, Clone)]
 pub struct LandBank {
@@ -51,50 +49,61 @@ pub struct DevFund {
     pub dev: Address,
 }
 
-async fn enable_provider() -> Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
-    let provider = Arc::new({
-        // connect to the network
-        let provider =
-            Provider::try_from(String::from(env::var("RPC_URL").expect("error"))).unwrap();
-        let chain_id = provider.get_chainid().await;
+// async fn enable_provider() -> Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
+//     let provider = Arc::new({
+//         // connect to the network
+//         let provider =
+//             Provider::try_from(String::from(env::var("RPC_URL").expect("error"))).unwrap();
+//         let chain_id = provider.get_chainid().await;
 
-        // this wallet's private key
-        let wallet = String::from(env::var("PRIVATE_KEY").expect("error"))
-            .parse::<LocalWallet>()
-            .expect("Unable to derive wallet")
-            .with_chain_id(chain_id.expect("msg").as_u64());
+//         // this wallet's private key
+//         let wallet = String::from(env::var("PRIVATE_KEY").expect("error"))
+//             .parse::<LocalWallet>()
+//             .expect("Unable to derive wallet")
+//             .with_chain_id(chain_id.expect("msg").as_u64());
 
-        SignerMiddleware::new(provider, wallet)
-    });
-    provider
-}
+//         SignerMiddleware::new(provider, wallet)
+//     });
+//     provider
+// }
 
-async fn land_contract() -> LandNFT<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
-    let land_address = String::from(env::var("LAND_CONTRACT").expect("error"))
-        .parse::<Address>()
-        .unwrap();
-    let land_contract = LandNFT::new(land_address, enable_provider().await);
+// async fn land_contract() -> LandNFT<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
+//     let land_address = String::from(env::var("LAND_CONTRACT").expect("error"))
+//         .parse::<Address>()
+//         .unwrap();
+//     // let land_address =
+//     let land_contract = LandNFT::new(land_address, enable_provider().await);
 
-    land_contract
-}
+//     land_contract
+// }
 
-async fn token() -> ERC20<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
-    let token_address = String::from(env::var("TOKEN_ADDRESS").expect("error"))
-        .parse::<Address>()
-        .unwrap();
+// async fn token() -> ERC20<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
+//     let token_address = String::from(env::var("TOKEN_ADDRESS").expect("error"))
+//         .parse::<Address>()
+//         .unwrap();
 
-    let token = ERC20::new(token_address, enable_provider().await);
+//     let token = ERC20::new(token_address, enable_provider().await);
 
-    token
-}
+//     token
+// }
+
+// async fn generate_contracts() -> 
+// let land_address = String::from(env::var("LAND_CONTRACT").expect("error"))
+// .parse::<Address>()
+// .unwrap();
+// let land_contract = LandNFT::new(land_address, provider);
+// let token_address = String::from(env::var("TOKEN_ADDRESS").expect("error"))
+// .parse::<Address>()
+// .unwrap();
+// let token = ERC20::new(token_address, provider);
 
 #[get("/balance_of")]
-async fn get_user_balance(user: web::Json<User>) -> impl Responder {
+async fn get_user_balance(user: web::Json<User>, data: web::Data<DataState>) -> impl Responder {
     let address = user.address;
 
-    let balanceOf = land_contract().await.balance_of(address);
+    let balance_of = data.land_contract.balance_of(address);
 
-    let result = balanceOf.call().await;
+    let result = balance_of.call().await;
 
     HttpResponse::Ok()
         .content_type(ContentType::json())
@@ -102,9 +111,9 @@ async fn get_user_balance(user: web::Json<User>) -> impl Responder {
 }
 
 #[post("/set_bank")]
-async fn set_land_bank(data: web::Json<DevFund>) -> impl Responder {
-    let new_landBank = data.dev;
-    let setter = land_contract().await.set_dev_fund(new_landBank);
+async fn set_land_bank(land_bank: web::Json<LandBank>, data: web::Data<DataState>) -> impl Responder {
+    let new_landBank = land_bank.bank;
+    let setter = data.land_contract.set_dev_fund(new_landBank);
 
     let tx = setter.send().await.unwrap();
     HttpResponse::Ok()
@@ -113,9 +122,9 @@ async fn set_land_bank(data: web::Json<DevFund>) -> impl Responder {
 }
 
 #[post("/set_dev")]
-async fn set_dev_fund(bank: web::Json<LandBank>) -> impl Responder {
+async fn set_dev_fund(bank: web::Json<LandBank>, data: web::Data<DataState>) -> impl Responder {
     let new_landBank = bank.bank;
-    let setter = land_contract().await.set_land_bank(new_landBank);
+    let setter = data.land_contract.set_land_bank(new_landBank);
 
     let tx = setter.send().await.unwrap();
     HttpResponse::Ok()
@@ -124,39 +133,19 @@ async fn set_dev_fund(bank: web::Json<LandBank>) -> impl Responder {
 }
 
 #[post("/mint")]
-async fn mint(field: web::Json<Region>) -> impl Responder {
-    // let rio_address = "0x32e0b53b799cc14c455011fe3458306f89aee848"
-    //     .parse::<Address>()
-    //     .unwrap();
-
-    // let rio = ERC20::new(rio_address, enable_provider().await);
-
-    // let wallet_address = "0x27a1876A09581E02E583E002E42EC1322abE9655"
-    //     .parse::<Address>()
-    //     .unwrap();
-    // let balance = rio.balance_of(wallet_address).call().await.unwrap();
-    // let price = land_contract().await.price().call().await.unwrap();
+async fn mint(field: web::Json<Region>, data: web::Data<DataState>) -> impl Responder
+{
     let price_u256 = U256::from(field.price);
     let region_u256 = field.region.iter().map(|x| U256::from(*x)).collect();
 
-    // println!(
-    //     "Wallet Balance is {:?}
-    //             \n Price is {:?}
-    //             \n Price of from json {:?}
-    //             \n Region Vec<u128> {:?}
-    //             \n Region Vec<U256> {:?}",
-    //     balance, price, price_u256, field.region, region_u256
-    // );
-
-    let approve = token()
-        .await
-        .approve(land_contract().await.address(), price_u256);
+    let approve = data.token
+        .approve(data.land_contract.address(), price_u256);
 
     approve.send().await.unwrap();
 
     thread::sleep(time::Duration::from_secs(20));
 
-    let buy_land = land_contract().await.mint(region_u256, price_u256);
+    let buy_land = data.land_contract.mint(region_u256, price_u256);
 
     let tx = buy_land.send().await.unwrap();
 
@@ -168,8 +157,8 @@ async fn mint(field: web::Json<Region>) -> impl Responder {
 }
 
 #[get("/total_supply")]
-async fn get_total_supply() -> impl Responder {
-    let total_supply = land_contract().await.total_supply().call().await;
+async fn get_total_supply(data: web::Data<DataState>) -> impl Responder {
+    let total_supply = data.land_contract.total_supply().call().await;
 
     HttpResponse::Ok()
         .content_type(ContentType::json())
@@ -181,13 +170,23 @@ async fn health_check() -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+pub fn run(
+    listener: TcpListener,
+    provider: Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>>,
+) -> Result<Server, std::io::Error> {
     dotenv().ok();
+    let land_address = String::from(env::var("LAND_CONTRACT").expect("error"))
+        .parse::<Address>()
+        .unwrap();
+    let land_contract = LandNFT::new(land_address, provider.clone());
+    let token_address = String::from(env::var("TOKEN_ADDRESS").expect("error"))
+        .parse::<Address>()
+        .unwrap();
+    let token = ERC20::new(token_address, provider.clone());
     let app_state = web::Data::new(DataState {
-        key: String::from(env::var("RPC_URL").expect("error")),
-        token: String::from(env::var("TOKEN_ADDRESS").expect("error")),
-        rpc_url: String::from(env::var("RPC_URL").expect("error")),
-        land_contract: String::from(env::var("LAND_CONTRACT").expect("error")),
+        token: token,
+        land_contract: land_contract,
+        provider: provider.clone(),
     });
     let server = HttpServer::new(move || {
         App::new()
--- app/tests/integration.rs
+++ app/tests/integration.rs
@@ -1,15 +1,45 @@
 //! tests/integration.rs
 
-use app::run;
+use dotenv::dotenv;
+use ethers::providers::Middleware;
+use ethers::signers::{LocalWallet, Signer};
+use ethers::{
+    prelude::{k256::ecdsa::SigningKey, SignerMiddleware},
+    providers::{Http, Provider},
+    signers::Wallet,
+    utils::Anvil,
+};
 use reqwest::Client;
-use std::net::TcpListener;
+use std::env;
+use std::time::Duration;
+use std::{net::TcpListener, sync::Arc};
 
 async fn spawn_app() -> String {
+    dotenv().ok();
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
-    let server = app::run(listener).expect("Failed to bind address");
+    let server = app::run(listener, enable_provider().await).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
+}
+
+async fn enable_provider() -> Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
+    let anvil = Anvil::new().spawn();
+    let provider = Arc::new({
+        let provider = Provider::<Http>::try_from(anvil.endpoint())
+            .expect("Unable to Create Provider")
+            .interval(Duration::from_millis(10u64));
+        let chain_id = provider.get_chainid().await;
+
+        // this wallet's private key
+        let wallet = String::from(env::var("PRIVATE_KEY").expect("error"))
+            .parse::<LocalWallet>()
+            .expect("Unable to derive wallet")
+            .with_chain_id(chain_id.expect("msg").as_u64());
+
+        SignerMiddleware::new(provider, wallet)
+    });
+    provider
 }
 
 #[tokio::test]
@@ -57,6 +87,8 @@ async fn get_balance() {
     assert!(response_result == "4");
 }
 
+#[tokio::test]
+
 async fn mint_works() {
     let address = spawn_app().await;
 
@@ -85,3 +117,41 @@ async fn mint_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
+
+// curl -v POST "http://127.0.0.1:8000/mint" -H 'Accept: application/json' -H 'Content-Type: application/json' -d '{
+
+//     "region": ["0xae0b5f5221983e3e82f0eb91f7345e7848dec067d9dc6a0d2eed5a41245d97f3"],
+//     "price": "0xae0b5f5221983e3e82f0eb91f7345e7848dec067d9dc6a0d2eed5a41245d97f3"
+//     }'
+
+//     curl -v POST "http://127.0.0.1:8000/mint" -H 'Accept: application/json' -H 'Content-Type: application/json' -d '{
+
+//         "region": ["0xae0b5f5221983e3e82f0eb91f7345e7848dec067d9dc6a0d2eed5a41245d97f3"],
+//         "price": "0xae0b5f5221983e3e82f0eb91f7345e7848dec067d9dc6a0d2eed5a41245d97f3"
+//         }'
+
+//     curl -v POST "http://127.0.0.1:8000/mint" -H 'Accept: application/json' -H 'Content-Type: application/json' -d '{
+
+//         "region": ["0"],
+//         "price": "200"
+//         }'
+
+//     curl -v POST "http://127.0.0.1:8000/mint" -H 'Accept: application/json' -H 'Content-Type: application/json' -d '{
+
+//         "region": [2],
+//         "price": 2000000000000000000
+//         }'
+
+//     curl -v POST "http://127.0.0.1:8000/balance_of" -H 'Accept: application/json' -H 'Content-Type: application/json' -d '{
+
+//         "address": "0x27a1876A09581E02E583E002E42EC1322abE9655"
+//         }'
+
+// 0x27a1876A09581E02E583E002E42EC1322abE9655
+
+//          curl -v POST "http://127.0.0.1:8000/total_supply"
+
+//          91000000000000000000000
+//          2000000000000000000
+
+//          curl -v POST "http://127.0.0.1:8000/total_supply"
