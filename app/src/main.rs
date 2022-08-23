e42a975f92339d3668f3c48b392046b56174840f
joshualyguessennd <joshualyguessennd@icloud.com>
2022-08-23 12:43:54 +0000

    fix: lint

--- app/src/main.rs
+++ app/src/main.rs
@@ -2,10 +2,10 @@ use app::run;
 use dotenv::dotenv;
 use env_logger::Env;
 use ethers::prelude::k256::ecdsa::SigningKey;
-use ethers::{prelude::*, providers::Provider, types::Address};
+use ethers::{prelude::*, providers::Provider};
 use std::env;
 use std::net::TcpListener;
-use std::sync::{Arc, Mutex};
+use std::sync::Arc;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
@@ -16,14 +16,14 @@ async fn main() -> std::io::Result<()> {
 }
 
 async fn enable_provider() -> Arc<SignerMiddleware<Provider<Http>, Wallet<SigningKey>>> {
-    dotenv().ok();
     let provider = Arc::new({
         // connect to the network
-        let provider = Provider::try_from(String::from(env::var("RPC_URL").unwrap())).unwrap();
+        let provider = Provider::try_from(env::var("RPC_URL").unwrap()).unwrap();
         let chain_id = provider.get_chainid().await;
 
         // this wallet's private key
-        let wallet = String::from(env::var("PRIVATE_KEY").expect("error"))
+        let wallet = env::var("PRIVATE_KEY")
+            .expect("error")
             .parse::<LocalWallet>()
             .expect("Unable to derive wallet")
             .with_chain_id(chain_id.expect("msg").as_u64());
