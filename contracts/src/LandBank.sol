b960a804b436481e306be49e55dc59baabb45536
joshualyguessennd <joshualyguessennd@icloud.com>
2022-09-04 13:02:00 +0000

    feat: function buyFromBank

--- contracts/src/LandBank.sol
+++ contracts/src/LandBank.sol
@@ -6,6 +6,7 @@ import {ReentrancyGuard} from "@rari-capital/solmate/src/utils/ReentrancyGuard.s
 import {ILandNFT} from "./interfaces/ILandNft.sol";
 
 import "./interfaces/ILandBank.sol";
+import "./interfaces/IUniswapV2Router.sol";
 
 error InsufficientRio();
 error InvalidLand();
@@ -15,10 +16,13 @@ contract LandBank is ReentrancyGuard {
     // RIO token address
     address private constant RIO_TOKEN =
         0xf21661D0D1d76d3ECb8e1B9F1c923DBfffAe4097;
+    address public constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
     address public owner;
     address public landNft;
     address public devFund;
+    address private constant UNISWAP_V2_ROUTER =
+        0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
 
     uint256 public landPrice;
 
@@ -70,14 +74,35 @@ contract LandBank is ReentrancyGuard {
         uint256 i;
         uint256 numberOfPx = _tokenIds.length;
         uint256 amountToSend = numberOfPx * landPrice;
+        address[] memory path = new address[](2);
+        path[0] = address(RIO_TOKEN);
+        path[1] = address(WETH);
+
+        // Approve the Uniswap Router contract
+
+        for (i; i < _tokenIds[i]; i++) {
+            if (timelapse[_tokenIds[i]] + 5 days < block.timestamp) {
+                revert coolDown();
+            }
+        }
         if (IERC20(RIO_TOKEN).balanceOf(msg.sender) < amountToSend) {
             revert InsufficientRio();
         }
         IERC20(RIO_TOKEN).transferFrom(msg.sender, address(this), amountToSend);
+        uint256 amountIn = (amountToSend * 20) / 100;
+        uint256 amountOutMin = getAmountOutMin(amountIn, path);
+        IERC20(RIO_TOKEN).approve(UNISWAP_V2_ROUTER, amountIn);
+        IUniswapV2Router(UNISWAP_V2_ROUTER).swapExactTokensForETH(
+            amountIn,
+            amountOutMin,
+            path,
+            devFund,
+            block.timestamp
+        );
         for (i; i < numberOfPx; i++) {
             ILandNFT(landNft).transferFrom(
-                msg.sender,
                 address(this),
+                msg.sender,
                 _tokenIds[i]
             );
         }
@@ -117,4 +142,13 @@ contract LandBank is ReentrancyGuard {
     //     );
     //     IERC20(RIO_TOKEN).transfer(_beneficiary, _amount);
     // }
+    function getAmountOutMin(uint256 _amountIn, address[] memory path)
+        public
+        view
+        returns (uint256)
+    {
+        uint256[] memory amountOutMins = IUniswapV2Router(UNISWAP_V2_ROUTER)
+            .getAmountsOut(_amountIn, path);
+        return amountOutMins[path.length - 1];
+    }
 }
