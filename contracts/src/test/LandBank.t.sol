49dc9fcad81c228def6019f8f389c0fccda6b7a3
joshualyguessennd <joshualyguessennd@icloud.com>
2022-09-16 22:21:11 +0000

    import format

--- contracts/src/test/LandBank.t.sol
+++ contracts/src/test/LandBank.t.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.8.15;
 
 import "forge-std/Test.sol";
-import "../LandBank.sol";
+import {LandBank} from "../LandBank.sol";
 import "../landNFT.sol";
 import {Utilities} from "./utils/Utilities.t.sol";
 import {ILandNFT} from "../interfaces/ILandNft.sol";
