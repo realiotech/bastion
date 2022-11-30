4285fe68b2ff2e9a8d1c5da804c75424c5724ab0
joshualyguessennd <joshualyguessennd@icloud.com>
2022-11-30 08:55:25 +0000

    compiled script

---
+++ contracts/src/errors.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.15;
+
+error InsufficientRio();
+error InsufficientEthBalance();
+error InvalidLand();
+error coolDown();
+error FailedTransfer();
+error CannotSetAddressZero();
+error NoTilesSelected();
+error RegionAlreadyOwned();
+error NotAuthorised();
+error ComissionOutOfAllowedRange();
+error InsufficientBalance();
+error InvalidToken();
+error NonExistentTokenURI();
+error TransferFailed();
+error MaxTilesReached();
+error UnauthorizedToMint();
+error AddressAlreadyClaimed();
--- contracts/src/scripts/LandNFT.s.sol
+++ contracts/src/scripts/LandNFT.s.sol
@@ -2,15 +2,18 @@
 pragma solidity >=0.8.15;
 
 import "../LandNFT.sol";
+import "../LandBank.sol";
 import "forge-std/Script.sol";
 
 contract DeploymentLandNFT is Script {
     address public constant dev = 0x27a1876A09581E02E583E002E42EC1322abE9655;
-    uint256 public price = 0.01 ether;
+    uint256 public constant price = 0.01 ether;
 
     function run() external {
         vm.startBroadcast();
-        new LandNFT(dev, price);
+        LandNFT nft = new LandNFT(dev, price);
+        LandBank bank = new LandBank(dev, address(nft));
+        nft.setLandBank(payable(address(bank)));
         vm.stopBroadcast();
     }
 }
